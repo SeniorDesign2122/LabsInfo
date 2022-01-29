81f0a16789b81b2345c67355cb5a62693bacb3ed
Salman Khan <muhammadsalman.khan@wmich.edu>
2022-01-28 22:38:08 -0500

    Updated manifest, gradle, strings.xml, getDetails(); Created DetailActivity.java and detailsLoaded()

---
+++ app/src/main/java/wmu/parkview/labsinfo/DetailActivity.java
@@ -0,0 +1,9 @@
+package wmu.parkview.labsinfo;
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
+
+public class DetailActivity extends YouTubeBaseActivity {
+    void detailsLoaded() {
+
+    }
+}
