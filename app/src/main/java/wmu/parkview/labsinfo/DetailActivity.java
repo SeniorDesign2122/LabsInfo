042ebd160447a0dbd2515657fa6e5356c42fb216
Salman Khan <muhammadsalman.khan@wmich.edu>
2022-01-28 22:48:28 -0500

    Added doc comments to DetailActivity.java and detailsLoaded()

--- app/src/main/java/wmu/parkview/labsinfo/DetailActivity.java
+++ app/src/main/java/wmu/parkview/labsinfo/DetailActivity.java
@@ -2,8 +2,17 @@ package wmu.parkview.labsinfo;
 
 import com.google.android.youtube.player.YouTubeBaseActivity;
 
+/**
+ * This activity gets details for the current qr string and the title received as an extra and
+ * adds them to its linear layout
+ */
 public class DetailActivity extends YouTubeBaseActivity {
+
+    /**
+     * Expects to be called by DBHelper when details loaded from database are ready to use
+     */
     void detailsLoaded() {
 
     }
+
 }
