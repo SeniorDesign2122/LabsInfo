0ae48c49a08e4dffc1fd9d7b2d145e2d5acce58e
matthew <matthew.probosutedjo@wmich.edu>
2022-01-28 23:20:32 -0500

    Created ListActivity.java and titlesLoaded() and Updated getTitles()

---
+++ app/src/main/java/wmu/parkview/labsinfo/ListActivity.java
@@ -0,0 +1,17 @@
+package wmu.parkview.labsinfo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * This activity gets titles for the current qr string and adds them to its recycler view
+ */
+public class ListActivity extends AppCompatActivity {
+
+    /**
+     * Expects to be called by DBHelper when titles loaded from database are ready to use
+     */
+    void titlesLoaded() {
+
+    }
+
+}
