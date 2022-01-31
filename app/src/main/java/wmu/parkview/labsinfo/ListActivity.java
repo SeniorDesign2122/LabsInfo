2f470c0b9b256010ce0e3b36bee8822ed3cb8aec
Salman Khan <muhammadsalman.khan@wmich.edu>
2022-01-31 02:07:52 -0500

    Created activity_list.xml, Overrode onCreate() and Updated manifest

--- app/src/main/java/wmu/parkview/labsinfo/ListActivity.java
+++ app/src/main/java/wmu/parkview/labsinfo/ListActivity.java
@@ -1,11 +1,35 @@
 package wmu.parkview.labsinfo;
 
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
 
 /**
  * This activity gets titles for the current qr string and adds them to its recycler view
  */
 public class ListActivity extends AppCompatActivity {
+    private RecyclerView mRView;
+
+    private List<String> mTitles;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        if (getIntent().getStringExtra("qRString") != null)
+            DBHelper.currentQRString = getIntent().getStringExtra("qRString");
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+
+        mRView = findViewById(R.id.recycler_view);
+        mRView.setLayoutManager(new LinearLayoutManager(this));
+
+        mTitles = DBHelper.getTitles(this, null);
+    }
 
     /**
      * Expects to be called by DBHelper when titles loaded from database are ready to use
@@ -13,5 +37,4 @@ public class ListActivity extends AppCompatActivity {
     void titlesLoaded() {
 
     }
-
 }
