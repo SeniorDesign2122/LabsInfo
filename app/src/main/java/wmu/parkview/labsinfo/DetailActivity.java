195ef9d24b6feff313552333a662d2553a0ddcd4
shourav-100 <56369953+shourav-100@users.noreply.github.com>
2022-01-31 15:43:37 -0500

    Overrode onCreate()

--- app/src/main/java/wmu/parkview/labsinfo/DetailActivity.java
+++ app/src/main/java/wmu/parkview/labsinfo/DetailActivity.java
@@ -1,12 +1,34 @@
 package wmu.parkview.labsinfo;
 
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import androidx.annotation.Nullable;
+
 import com.google.android.youtube.player.YouTubeBaseActivity;
+
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * This activity gets details for the current qr string and the title received as an extra and
  * adds them to its linear layout
  */
 public class DetailActivity extends YouTubeBaseActivity {
+    private LinearLayout mLVLayout;
+
+    private List<HashMap<String, String>> mDetails;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        mLVLayout = findViewById(R.id.detail_vertical_layout);
+
+        mDetails = DBHelper.getDetails(this, getIntent().getStringExtra("title"),
+                null);
+    }
 
     /**
      * Expects to be called by DBHelper when details loaded from database are ready to use
